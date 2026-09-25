ac25b282bc30215f12aba83ce952fbe4351dd197
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2026-09-25 14:20:21 +0200

    .

--- modules/VisitLog/Ajax/VisitLogAjax.php
+++ modules/VisitLog/Ajax/VisitLogAjax.php
@@ -9,27 +9,4 @@ class VisitLogAjax extends \Core\AjaxController
         $VisitLog = new \VisitLog\VisitLog();
         return $VisitLog->getDataTable($options);
     }
-
-    public function update($data)
-    {
-        $this->will('VisitLog', 'edit');
-        $VisitLog = new \VisitLog\VisitLog();
-        $VisitLog->update($data->id, $data);
-    }
-    
-    public function updateMultiple(array $data)
-    {      
-        $this->will('VisitLog', 'edit');
-        $VisitLog = new \VisitLog\VisitLog();
-        foreach ($data as $row) {
-            $VisitLog->update($row->id, $row->data);
-        }
-    }
-
-    public function insert($data)
-    {
-        $this->will('VisitLog', 'add');
-        $VisitLog = new \VisitLog\VisitLog();
-        $id = $VisitLog->insert($data);
-    }
 }
